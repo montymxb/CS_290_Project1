99e25f0d090cfc0bd82092c758a9bfd941480406
80years80 <akbulute@oregonstate.edu>
2018-08-12 11:41:53 -0700

    updated server.js slightly

--- routes/login.js
+++ routes/login.js
@@ -10,7 +10,7 @@ function onSignIn(googleUser) {
   
   var id_token = googleUser.getAuthResponse().id_token;
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'http://localhost:5000/login');
+  xhr.open('POST', 'http://localhost:5555/login');
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.onload = function() {
   console.log('Signed in as: ' + xhr.responseText);
@@ -18,18 +18,3 @@ function onSignIn(googleUser) {
   xhr.send('idtoken=' + id_token);
 }
 
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
-  const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: 1052113773074-v9n86g17t7kc4e6bf3g2u0j37qvaig1s.apps.googleusercontent.com,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  //const domain = payload['hd'];
-}
-verify().catch(console.error);
